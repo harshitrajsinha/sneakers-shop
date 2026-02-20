5a01aeb2b124a53317e78be5fded06a29ad2e10d
harshitrajsinha <raj.harshitsinha08@gmail.com>
2026-02-20 17:02:39 +0000

    fix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -81,8 +81,8 @@ export default {
   methods: {
     async loadProducts() {
       try {
-        // const response = await axios.get(`http://${process.env.VUE_APP_SERVER_IP}:8080/api/products`);
-        const response = await axios.get(`http://localhost:8080/api/products`);
+        const response = await axios.get(`http://${import.meta.env.VUE_APP_SERVER_IP}:8080/api/products`);
+        // const response = await axios.get(`http://localhost:8080/api/products`);
         console.log("here again", import.meta.env.VITE_SERVER_IP)
         this.products = response.data;
         this.loading = false;
@@ -94,8 +94,8 @@ export default {
     async showProductDetails(productId) {
       try {
         const response = await axios.get(
-          // `http://${process.env.VUE_APP_SERVER_IP}:8080/api/products/${productId}`
-           `http://localhost:8080/api/products/${productId}`
+          `http://${process.env.VUE_APP_SERVER_IP}:8080/api/products/${productId}`
+          // `http://localhost:8080/api/products/${productId}`
         );
         this.selectedProductDetails = response.data;
         this.showModal = true;
@@ -112,8 +112,8 @@ export default {
         };
 
         const response = await axios.post(
-          // "http://${process.env.VUE_APP_SERVER_IP}:8081/api/payment/process",
-          "http://localhost:8081/api/payment/process",
+          "http://${process.env.VUE_APP_SERVER_IP}:8081/api/payment/process",
+          // "http://localhost:8081/api/payment/process",
           paymentData
         );
 
